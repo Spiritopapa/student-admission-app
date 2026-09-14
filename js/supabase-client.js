829bf8738fe4a85b48facbda624d905b2bc0d3a3
Spiritopapa <Spiritopapa@users.noreply.github.com>
2026-09-14 06:33:59 +0000

    modified

--- js/supabase-client.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://rfsfkxplafwlmwncwduz.supabase.co'
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_9iTTz6D1zvaEZIsauboxKQ__wyPfK5A'
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
-)
